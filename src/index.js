4396e2df2872911357f99d0fbc8d873073853e1a
Haritha Rathinakumar <hrathin@g.clemson.edu>
2018-07-11 10:31:54 -0400

    Setup react app

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <p>App</p>,
+  document.getElementById("app")
+);
+
+module.hot.accept();
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.js'
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  output: {
+    path: __dirname + './',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './',
+    historyApiFallback: true,
+    hot: true,
+    progress: true,
+    port: 3000
+  },
+  mode: 'development'
+};
